4a47ac72607e4ebcde59415bf080cd046882d972
Julian-HerreraPerez <jah3290@g.rit.edu>
2025-12-08 12:05:28 -0500

    project 3 work

--- project3/script.js
+++ project3/script.js
@@ -1,3 +1,6 @@
+
+
+
 window.addEventListener("keydown", function (event) {
         if (event.defaultPrevented) {
             return;
@@ -5,14 +8,24 @@ window.addEventListener("keydown", function (event) {
         
 
         let player = document.getElementById("player");
-        let currentX = parseInt(player.style.left) || 0;
+        let x = parseInt(player.style.left) || 0;
+        let area = document.getElementById("gamearea");
+
+        let pWidth = player.offsetWidth;
+        let aWidth = area.clientWidth/2;
+  
+
 
          switch (event.key) {
             case "ArrowLeft":
-                player.style.left= parseInt(currentX -5) + "px";
+                x -= 5;
                 break;
             case "ArrowRight":
-                player.style.left= parseInt(currentX +5) + "px";
+                x+=5;
                 break;
         }
+
+
+        x = Math.max(-aWidth, Math.min(x, aWidth));
+        player.style.left= x + "px";
     })
