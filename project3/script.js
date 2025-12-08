03e5d9cd9c81666a1361074af061606dabf63130
Julian-HerreraPerez <jah3290@g.rit.edu>
2025-12-08 09:48:12 -0500

    project 3

--- project3/script.js
+++ project3/script.js
@@ -1,22 +1,18 @@
-const dx = 100;
-let x = 480 / 2;
-
 window.addEventListener("keydown", function (event) {
         if (event.defaultPrevented) {
             return;
         }
         
 
-        let player = document.querySelectorAll("#player");
+        let player = document.getElementById("player");
+        let currentX = parseInt(player.style.left) || 0;
 
          switch (event.key) {
             case "ArrowLeft":
-                x-=dx;
-                player.x = x;
+                player.style.left= parseInt(currentX -5) + "px";
                 break;
             case "ArrowRight":
-                x+=dx;
-                player.x = x;
+                player.style.left= parseInt(currentX +5) + "px";
                 break;
         }
     })
