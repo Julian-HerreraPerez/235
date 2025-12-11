3d9a13656e5746c7a91cdf5e3f5ddaf21900f225
Julian <jah3290@g.rit.edu>
2025-12-11 14:38:45 -0500

    project 3

--- project3/script.js
+++ project3/script.js
@@ -1,12 +1,30 @@
-let spawnLine = 25;
-let spawnRate = 1500;
-let spawnRateOfDescent = 0.50;
+const spawnLine = 25;
+const maxY = 850;
+const spawnRate = 1500;
+const spawnRateOfDescent = 0.75;
 let lastSpawn = -1;
-let objects = [];
-let gameArea = document.getElementById("gamearea");
-let player = document.getElementById("player");
+const objects = [];
+let animationId;
+const gameArea = document.getElementById("gamearea");
+const player = document.getElementById("player");
+const scoreText = document.getElementById("score");
+
+let score = 0;
+let savedHighScore = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+    let highScore = document.getElementById("highScore");
+
+    const saved = localStorage.getItem('HighScore');
+    if (saved) {
+        savedHighScore = parseInt(saved);
+        highScore.innerHTML = "HighScore: " + savedHighScore;
+    }
+})
+
 
 requestAnimationFrame(animate);
+
 
 window.addEventListener("keydown", function (event) {
     if (event.defaultPrevented) {
@@ -15,6 +33,10 @@ window.addEventListener("keydown", function (event) {
 
     let x = parseInt(player.style.left) || 0;
     let area = document.getElementById("gamearea");
+
+    if (!area) {
+        return;
+    }
 
     let aWidth = area.clientWidth / 2;
 
@@ -43,9 +65,14 @@ function spawnRandomObject() {
         randomObject = "blue";
     }
 
-    let element = document.createElement("div");
+    let element = document.createElement("img");
     element.classList.add("object");
-    element.style.background = randomObject;
+    if (randomObject == "red") {
+        element.src = "oil.png";
+    }
+    else {
+        element.src = "jerrycan.png";
+    }
 
 
     let xPosition = Math.random() * (gameArea.clientWidth - 30) + 15;
@@ -55,17 +82,13 @@ function spawnRandomObject() {
 
     gameArea.appendChild(element);
 
-    objects.push({
-        type: randomObject,
-        element: element,
-        x: xPosition,
-        y: spawnLine
-    });
+    objects.push(new spawningObjects(randomObject, element, xPosition, spawnLine));
 }
 
 function animate() {
-    let now = Date.now();
+    animationId = requestAnimationFrame(animate);
 
+    let now = Date.now();
     if (now > lastSpawn + spawnRate) {
         lastSpawn = now;
         spawnRandomObject();
@@ -73,14 +96,68 @@ function animate() {
 
     for (let i = 0; i < objects.length; i++) {
         let currentObject = objects[i];
-        currentObject.y += spawnRateOfDescent;
-        currentObject.element.style.top = currentObject.y + "px";
+        currentObject.move(spawnRateOfDescent)
+
+        if (currentObject.y > maxY) {
+            objects.splice(i, 1);
+            currentObject.element.remove();
+            i--;
+        }
+        if (collisionDetection(currentObject.element, player)) {
+            objects.splice(i, 1);
+            currentObject.element.remove();
+            i--;
+
+            if (currentObject.type == "blue") {
+                console.log("blue")
+                score++;
+                scoreText.textContent = "Score: " + score;
+            }
+            else if (currentObject.type == "red") {
+                gameOver("You hit oil and spun out. Score: " + score)
+            }
+        }
     }
-
-
-    requestAnimationFrame(animate);
 }
 
-function collisionDetection() {
+function collisionDetection(gameObj, player) {
+    const obj = gameObj.getBoundingClientRect();
+    const car = player.getBoundingClientRect();
 
+    return !(obj.bottom < car.top ||
+        obj.top > car.bottom ||
+        obj.right < car.left ||
+        obj.left > car.right
+    );
+}
+
+
+
+function gameOver(message) {
+    cancelAnimationFrame(animationId);
+    if (score > savedHighScore) {
+        localStorage.setItem('HighScore', score);
+    }
+    document.body.innerHTML = `<h1 id="resetText" style = "color: white; text-align center;">${message}</h1>`
+    document.body.innerHTML += `<button id="reset"> Play Again </button>`
+    document.getElementById("reset").addEventListener("click", reset);
+
+}
+
+function reset() {
+    window.location.reload();
+}
+
+class spawningObjects {
+    constructor(type, element, x, y) {
+        this.type = type;
+        this.element = element;
+        this.x = x;
+        this.y = y;
+    }
+
+    move(rate) {
+        this.y += rate;
+        this.element.style.top = this.y + "px";
+    }
 }
